008843e82bb9beb298393643d3f7d594830ee142
NYX development <165662208+NYXdevelopments@users.noreply.github.com>
2024-04-12 21:04:12 +0330

    Delete Config.lua

--- Config.lua
+++
@@ -1,7 +0,0 @@
-Config = {}
-
-Config.limit = 8 -- how many numbers you can put in the payphone
-
-Config.pay = 100 -- how much money you need to pay per minute
-
-Config.moneytype = 'cash' -- you can put cash or bank to use one of them remove
